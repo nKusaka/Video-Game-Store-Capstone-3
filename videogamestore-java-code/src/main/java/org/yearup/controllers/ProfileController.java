4cde5f67cf37705d8518ba9cb7eb49a76cc8fd9e
Nik <nikkusaka@gmail.com>
2025-12-17 19:30:07 -0800

    -users can now view and update their profiles on the website

---
+++ videogamestore-java-code/src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,60 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.*;
+
+import java.security.Principal;
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+@PreAuthorize("hasRole('ROLE_USER')")
+public class ProfileController {
+
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("")
+    public Profile getProfile(Principal principal) {
+        try {
+            String userName = principal.getName();
+
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            return profileDao.getByUserId(userId);
+
+        } catch(Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+
+    @PutMapping("")
+    public Profile updateProfile(Principal principal, @RequestBody Profile profile) {
+        try {
+            String userName = principal.getName();
+
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            return profileDao.updateByUserId(userId, profile);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
+}
--- videogamestore-java-code/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ videogamestore-java-code/src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -1,5 +1,6 @@
 package org.yearup.data.mysql;
 
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
 import org.springframework.stereotype.Component;
 import org.yearup.models.Profile;
 import org.yearup.data.ProfileDao;
@@ -8,21 +9,17 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 @Component
-public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
-{
-    public MySqlProfileDao(DataSource dataSource)
-    {
+public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao {
+    public MySqlProfileDao(DataSource dataSource) {
         super(dataSource);
     }
 
     @Override
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
                 " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try(Connection connection = getConnection())
-        {
+        try (Connection connection = getConnection()) {
             PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             ps.setInt(1, profile.getUserId());
             ps.setString(2, profile.getFirstName());
@@ -37,11 +34,76 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
             ps.executeUpdate();
 
             return profile;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public Profile getByUserId(int userId) {
+        String sql = "SELECT * FROM videogamestore.profiles " +
+                "WHERE user_id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, userId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Profile profile = new Profile(
+                            resultSet.getInt("user_id"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("email"),
+                            resultSet.getString("address"),
+                            resultSet.getString("city"),
+                            resultSet.getString("state"),
+                            resultSet.getString("zip")
+
+                    );
+                    return profile;
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Error trying to fetch user profile " +
+                    e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public Profile updateByUserId(int userId, Profile profile) {
+        String sql = """
+                UPDATE videogamestore.profiles
+                SET first_name = ?,
+                    last_name = ?,
+                    phone = ?,
+                    email = ?,
+                    address = ?,
+                    city = ?,
+                    state = ?,
+                    zip = ?
+                WHERE user_id = ?""";
+
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, profile.getFirstName());
+            preparedStatement.setString(2, profile.getLastName());
+            preparedStatement.setString(3, profile.getPhone());
+            preparedStatement.setString(4, profile.getEmail());
+            preparedStatement.setString(5, profile.getAddress());
+            preparedStatement.setString(6, profile.getCity());
+            preparedStatement.setString(7, profile.getState());
+            preparedStatement.setString(8, profile.getZip());
+            preparedStatement.setInt(9, userId);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error updating user profile " +
+                    e.getMessage());
+        }
+        return profile;
     }
 
 }
